4ae313d83cf8b486a096ab60780ae52572679697
OlehScherbak <103534824+OlehScherbak@users.noreply.github.com>
2023-01-27 16:05:58 +0200

    unified count method

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,36 +4,43 @@ class Feedback extends React.Component{
     state = {
         good: 0,
         neutral: 0,
-        bad:0,
+        bad: 0,
     }
     
-    countGood = () => {
-        console.log(this.state.good);
+    feedbackCount = (evt) => {
+        console.log(evt.target.name);
         this.setState(prevState => ({
-            good: prevState.good + 1,
+            [evt.target.name] : prevState[evt.target.name] + 1,
         }));
     };
 
-    countNeutral = () => {
-        this.setState(prevState => ({
-            neutral: prevState.neutral + 1,
-        }));
-    };
+    // countGood = (evt) => {
+    //     console.log(evt.target.name);
+    //     this.setState(prevState => ({
+    //         good: prevState.good + 1,
+    //     }));
+    // };
+
+    // countNeutral = () => {
+    //     this.setState(prevState => ({
+    //         neutral: prevState.neutral + 1,
+    //     }));
+    // };
     
-    countBad = () => {
-        this.setState(prevState => ({
-            bad: prevState.bad + 1,
-        }));
-    };
+    // countBad = () => {
+    //     this.setState(prevState => ({
+    //         bad: prevState.bad + 1,
+    //     }));
+    // };
 
     render() {
         return (
             <div className="feedback">
                 <div className="feedbackControls">
                     <h2 className="title">Please leave feedback</h2>
-                    <button type="button" onClick={this.countGood}>Good</button>
-                    <button type="button" onClick={this.countNeutral}>Neutral</button>
-                    <button type="button" onClick={this.countBad}>Bad</button>
+                    <button type="button" name="good" onClick={this.feedbackCount}>Good</button>
+                    <button type="button" name="neutral" onClick={this.feedbackCount}>Neutral</button>
+                    <button type="button" name="bad" onClick={this.feedbackCount}>Bad</button>
                 </div>
                 <div className="statistics">
                     <h2 className="title">Statistics</h2>
